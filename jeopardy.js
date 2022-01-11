60459d97c4b6675728fcd8b18e8bf18c510d13d0
cng008 <92824407cng008@users.noreply.github.com>
2022-01-11 13:41:57 -0800

    add game board, styling

--- jeopardy.js
+++ jeopardy.js
@@ -17,21 +17,22 @@
 //    },
 //    ...
 //  ]
-
-let categories = [];
+const numOfCategories = 6;
+const numOfClues = 5;
+const categories = [];
 
 /** Get NUM_CATEGORIES random category from API. */
 async function getCategoryIds() {
   const res = await axios.get(`https://jservice.io/api/categories?count=100`);
   const catIds = res.data.map(result => result.id);
-  return _.sampleSize(catIds, 6); //Returns an array of 6 randomly picked categories
+  return _.sampleSize(catIds, numOfCategories); //Returns an array of 6 randomly picked categories
 }
 
 /** Returns object with data about a category: */
 async function getCategory(catId) {
   const res = await axios.get(`https://jservice.io/api/category?id=${catId}`);
   let allClues = res.data.clues;
-  let randomClues = _.sampleSize(allClues, 5); //randomly selects 5 questions for category
+  let randomClues = _.sampleSize(allClues, numOfClues); //randomly selects 5 questions for category
   let clues = randomClues.map(clue => ({
     question: clue.question,
     answer: clue.answer,
@@ -48,6 +49,25 @@ async function getCategory(catId) {
  *   (initally, just show a "?" where the question/answer would go.)
  */
 async function fillTable() {
+  $('#board')
+    .append($('<thead>').attr('id', 'header'))
+    .append($('<tbody>').attr('id', 'clues'));
+
+  // Add header row for numOfCategories
+  for (let x = 0; x < numOfCategories; x++) {
+    $('thead').append($('<th>').attr('id', x).text('category'));
+  }
+  $('#board').append($('thead'));
+
+  // creates main board based on set numOfCategories (WIDTH) x numOfClues (HEIGHT)
+  // creates a tr until the loop has iterated Y-times
+  for (let y = 0; y < numOfClues; y++) {
+    $('tbody').append($('<tr>').attr('id', `tier${y}`));
+    // creates a td and appends it to the tr above, loops until itiration is equal to numOfCategories (width)
+    for (let x = 0; x < numOfCategories; x++) {
+      $(`#tier${y}`).append($('<td>').attr('id', `clue${y}-cat${x}`).text('?'));
+    }
+  }
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -80,5 +100,17 @@ async function setupAndStart() {
 }
 
 /** TODO: On click of start / restart button, set up game. */
+//START GAME ON BUTTON CLICK
+$('#startGame').one('click', () => {
+  $('#game').show();
+  fillTable();
+  $('#startGame').text('Reset Game');
+});
+
+// RESTART GAME BUTTON
+$('#startGame').addEventListener('click', () => {
+  $('#board').empty();
+  fillTable();
+});
 
 /** TODO: On page load, add event handler for clicking clues */
